5d6320efa66a291571b343bfad7474a93f3430b0
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-07-26 20:37:44 +0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_25_172122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotes", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
